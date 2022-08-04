1c3afba5093074b126f641a8f6a958441e65452e
Jake <gmhelper@gmail.com>
2022-08-04 15:18:57 -0400

    Home Component Works

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
+import { HomeComponent } from './home/home.component';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: '', pathMatch: 'full', redirectTo: '/home'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
