b2352ce57f1f401da7e91283cad158757ce2c1a1
Jake <gmhelper@gmail.com>
2022-08-26 00:32:40 -0400

    Added AOS

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import * as AOS from 'aos';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'profile-website';
+
+  ngOnInit(): void {
+    AOS.init();
+  }
 
   constructor() {}
 
