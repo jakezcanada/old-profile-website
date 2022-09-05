46d909c9d34084ad717ffc4da4fac35521e86a95
Jake <gmhelper@gmail.com>
2022-09-05 13:02:37 -0400

    Created Home section

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+// @ts-ignore
+import Typewriter from 't-writer.js';
 
 @Component({
   selector: 'app-home',
@@ -10,6 +12,33 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    const target = document.querySelector('.tw')
+
+    const options = {
+      loop: true
+    }
+
+    const writer = new Typewriter(target, {
+      loop: true,
+      typeSpeed: 100,
+      deleteSpeed: 20,
+      typeColor: 'red'
+    })
+
+    writer
+      .type('Web Development')
+      .rest(500)
+      .remove(11)
+      .type('Design')
+      .rest(500)
+      .clear()
+      .type('Frontend Development')
+      .rest(500)
+      .clear()
+      .type('Backend Development')
+      .rest(500)
+      .clear()
+      .start()
   }
 
 }
